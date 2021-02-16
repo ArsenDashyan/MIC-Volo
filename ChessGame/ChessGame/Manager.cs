0a66fee4f641929c4a2f724929a0d7e6977008ca
ArsenDashyan <77542040+ArsenDashyan@users.noreply.github.com>
2021-02-17 00:01:31 +0400

    Added two methods for a queen occupancy dangerous

--- ChessGame/ChessGame/Manager.cs
+++ ChessGame/ChessGame/Manager.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading;
 
 namespace ChessGame
@@ -30,7 +29,7 @@ namespace ChessGame
 
                 while (count <= 4)
                 {
-                    Thread.Sleep(1000);
+                    Thread.Sleep(1500);
                     View.ShowBoard(tuple.Item1, tuple.Item2, count);
                     if (count != 4)
                     {
@@ -77,11 +76,13 @@ namespace ChessGame
             Console.WriteLine("Please enter a number coordinate");
             Console.SetCursorPosition(40, 3);
             int b = int.Parse(Console.ReadLine());
-            
-            if ((aFirst > queen.FCoord && b < rookL.SCoord && b < queen.SCoord ))
+
+            if (aFirst > queen.FCoord && b < rookL.SCoord && b < queen.SCoord
+                && GetLeftIndex(queen.FCoord, queen.SCoord, aFirst, b) && GetRightIndex(queen.FCoord, queen.SCoord, aFirst, b))
                 return (aFirst, b);
 
-            while (!(aFirst > queen.FCoord && b < rookL.SCoord && b < queen.SCoord))
+            while (!(aFirst > queen.FCoord && b < rookL.SCoord && b < queen.SCoord
+                && GetLeftIndex(queen.FCoord, queen.SCoord, aFirst, b) && GetRightIndex(queen.FCoord, queen.SCoord, aFirst, b)))
             {
                 Console.SetCursorPosition(40, 4);
                 Console.WriteLine("Please enter a correct coordinats");
@@ -126,6 +127,80 @@ namespace ChessGame
                     return 8;
             }
             return 0;
+        }
+        /// <summary>
+        /// Ստեղծում է սպիտակ թագուհու դեպի աջ անկյունագծային հարվածի տիրույթը
+        /// </summary>
+        /// <param name="a">Սպիտակ թագուհու սկզբնական առաջին կոորդինատ</param>
+        /// <param name="b">Սպիտակ թագուհու սկզբնական երկրորդ կոորդինա</param>
+        /// <param name="x">Սև արքայի մուտքային առաջին կոորդինատ</param>
+        /// <param name="y">Սև արքայի մուտքային երկրորդ կոորդինատ</param>
+        /// <returns>Վերադարձնում է true, եթե արքայի մուտքային կոորդինատները սպիտակ թագուգու աջ անկյունագծի վրա են</returns>
+        public static bool GetRightIndex(int a, int b, int x, int y)
+        {
+            (int, int)[] arri = new (int, int)[13];
+            bool result = true;
+            int sum = a + b;
+            int count = 0;
+
+            for (int i = 1; i < 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    if (i + j == sum)
+                    {
+                        arri[count] = (i, j);
+                        count++;
+                    }
+                }
+            }
+            arri = arri.Where(c => c.Item1 != 0 && c.Item2 != 0).ToArray();
+            for (int i = 0; i < arri.Length; i++)
+            {
+                if ((x, y) == arri[i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Ստեղծում է սպիտակ թագուհու դեպի ձախ հարվածի տիրույթը
+        /// </summary>
+        /// <param name="a">Սպիտակ թագուհու սկզբնական առաջին կոորդինատ</param>
+        /// <param name="b">Սպիտակ թագուհու սկզբնական երկրորդ կոորդինատ</param>
+        /// <param name="x">Սև արքայի մուտքային առաջին կոորդինատ</param>
+        /// <param name="y">Սև արքայի մուտքային երկրորդ կոորդինատ</param>
+        /// <returns>Վերադարձնում է true, եթե արքայի մուտքային կոորդինատները սպիտակ թագուգու ձախ անկյունագծի վրա են</returns>
+        public static bool GetLeftIndex(int a, int b, int x, int y)
+        {
+            (int, int)[] arri = new (int, int)[13];
+            int sub = Math.Abs(a - b);
+            int count = 0;
+            bool result = true;
+
+            for (int i = 1; i < 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    if (i - j == sub)
+                    {
+                        arri[count] = (j, i);
+                        count++;
+                    }
+                }
+            }
+            arri = arri.Where(c => c.Item1 != 0 && c.Item2 != 0).ToArray();
+            for (int i = 0; i < arri.Length; i++)
+            {
+                if ((x, y) == arri[i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
         }
     }
 }
--- ChessGame/ChessGame/Program.cs
+++ ChessGame/ChessGame/Program.cs
@@ -10,5 +10,7 @@ namespace ChessGame
             
             Console.ReadLine();
         }
+
+       
     }
 }
